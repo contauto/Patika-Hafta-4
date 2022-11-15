6cdf028d6a3a243db3665031245132340f335de4
contauto <controleng@yandex.com>
2022-11-15 18:38:21 +0300

    *

---
+++ Dizi-Yakin/src/Dizi_Yakin.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Dizi_Yakin {
+    public static void En_Yakin(int sayi) {
+        int[] dizi = {15, 12, 788, 1, -1, -778, 2, 0};
+        Arrays.sort(dizi);
+        int min=dizi[0];
+        int max=dizi[7];
+
+
+        for (int i : dizi
+        ) {
+            if ((i>min)&&(i<sayi)){
+                min=i;
+            }
+            else if((i<max)&&(i>sayi)){
+                max=i;
+            }
+        }
+
+        System.out.println("Girilen sayı:" + sayi + "\n" + "Girilen sayıdan küçük en yakın sayı: " + min + "\n" + "Girilen sayıdan büyük en yakın sayı: " + max);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    Ort.Harmonik();
+    //Ort.Harmonik();
+        Dizi_Yakin.En_Yakin(16);
     }
 }
